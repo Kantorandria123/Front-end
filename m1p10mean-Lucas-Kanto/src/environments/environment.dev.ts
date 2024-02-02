d20ed73fc06c2494e2948bcda8f4e76d4eff1e41
Kantorandria123 <kantomiharintsoarandria@gmail.com>
2024-02-02 10:11:48 +0300

    deleted

--- m1p10mean-Lucas-Kanto/src/environments/environment.dev.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-	production: false,
-  BASE_URL: '',
-  voiture_url: '/voitures',
-  fiche_url: '/fiches',
-  user_url: '/users' ,
-  typedepense_url: '/typedepenses' ,
-  finance_url: '/finances'
-}
