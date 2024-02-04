cc5165b0a4418cde7336cdcf332d4df28ec1890b
LucasRazafinjatovo <96420763+LucasRazafinjatovo@users.noreply.github.com>
2024-02-04 18:55:16 +0300

    [Service] liste service

--- m1p10mean-Lucas-Kanto/src/app/rendezvous/rendezvous.component.ts
+++ m1p10mean-Lucas-Kanto/src/app/rendezvous/rendezvous.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-rendezvous',
   templateUrl: './rendezvous.component.html',
-  styleUrl: './rendezvous.component.css'
+  styleUrls: ['./rendezvous.component.css']
 })
-export class RendezvousComponent {
+export class RendezvousComponent implements OnInit {
+  services: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    console.log("*************Rendez-vous******************");
+    this.getListService();
+  }
+
+  getListService() {
+    const url = 'http://localhost:3000/service/lesservices';
+
+    this.http.get<any>(url).subscribe(
+      (response) => {
+        // La réponse contient la liste des services
+        if (response.status && response.services) {
+          this.services = response.services;
+          console.log('Liste des services :', this.services);
+        } else {
+          console.error('Réponse inattendue du serveur :', response);
+        }
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération de la liste des services :', error);
+      }
+    );
+  }
 }
