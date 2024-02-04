885b993340bf2e038693b2b5eac3f429a8ae98cc
Kantorandria123 <kantomiharintsoarandria@gmail.com>
2024-02-04 23:36:40 +0300

    gestion-profil-employee

---
+++ BackOffice/src/app/profil-employee/profil-employee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profil-employee',
+  templateUrl: './profil-employee.component.html',
+  styleUrl: './profil-employee.component.css'
+})
+export class ProfilEmployeeComponent {
+
+}
