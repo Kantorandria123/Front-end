16eae15018d4fc8b266c8b3ca62b7c4602cd156a
LucasRazafinjatovo <96420763+LucasRazafinjatovo@users.noreply.github.com>
2024-02-04 10:50:35 +0300

    [Login] client

--- m1p10mean-Lucas-Kanto/src/app/header/header.component.ts
+++ m1p10mean-Lucas-Kanto/src/app/header/header.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  resultData: any;
+  constructor(private router: Router, private route: ActivatedRoute,private http: HttpClient, private cookieService: CookieService) { }
 
+  ngOnInit() {
+    console.log("*************hello******************");
+
+    const emailCookie = this.cookieService.get('email');
+    const tokenCookie = this.cookieService.get('token');
+
+    if (emailCookie && tokenCookie) {
+      let bodyData = {
+        email: emailCookie,
+        token: tokenCookie,
+      };
+
+      this.http.post("http://localhost:3000/client/getbytoken", bodyData).subscribe((resultData: any) => {
+        console.log("****azo eee****");
+        this.resultData=resultData;
+      });
+    } else {
+      // Les cookies n'existent pas, effectuez une autre action si nécessaire
+      console.log("Les cookies 'email' et 'token' n'existent pas.");
+    }
+  }
 }
--- m1p10mean-Lucas-Kanto/src/app/login/login.component.ts
+++ m1p10mean-Lucas-Kanto/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
 
 @Component({
   selector: 'app-login',
@@ -7,4 +11,37 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  email: string = 'john.doe@example.com';
+  mdp: string = '123';
+
+  isLogin: boolean = true;
+  erroMessage: string = "";
+
+  constructor(private router: Router,private http: HttpClient, private cookieService: CookieService) {}
+  login() {
+    console.log(this.email);
+    console.log(this.mdp);
+
+    let bodyData = {
+      email: this.email,
+      mdp: this.mdp,
+    };
+
+        this.http.post("http://localhost:3000/client/login", bodyData).subscribe(  (resultData: any) => {
+        console.log(resultData);
+
+        if (resultData.status)
+        {
+          this.cookieService.set('email', resultData.client.email);
+          this.cookieService.set('token', resultData.client.token);
+           this.router.navigateByUrl('/');
+        }
+        else
+         {
+          //alert("Incorrect Email or Password");
+          this.erroMessage="Incorrecte Email ou mot de passe";
+          console.log("Errror login");
+        }
+      });
+    }
 }
