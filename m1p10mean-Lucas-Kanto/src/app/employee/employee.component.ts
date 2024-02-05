e8e03a632f998fc001d93135112b6d3f43615202
Kantorandria123 <kantomiharintsoarandria@gmail.com>
2024-02-05 23:43:48 +0300

    ListEmployee

--- m1p10mean-Lucas-Kanto/src/app/employee/employee.component.ts
+++ m1p10mean-Lucas-Kanto/src/app/employee/employee.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './employee.component.css'
 })
 export class EmployeeComponent {
+  employes: any[] = [];
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.getListEmploye();
+  }
+  getListEmploye() {
+    const url = 'http://localhost:3000/employe/lesEmployes';
 
+    this.http.get<any>(url).subscribe(
+      (response) => {
+        if (response.status && response.employes) {
+          this.employes = response.employes;
+          console.log('Liste des services :', this.employes);
+        } else {
+          console.error('Réponse inattendue du serveur :', response);
+        }
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération de la liste des employes :', error);
+      }
+    );
+  }
 }
