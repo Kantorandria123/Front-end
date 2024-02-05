70211ac4b6f7d0d42210b2253a28e9f0e874a1c6
Kantorandria123 <kantomiharintsoarandria@gmail.com>
2024-02-05 14:52:32 +0300

    Inscription

--- m1p10mean-Lucas-Kanto/src/app/inscription/inscription.component.ts
+++ m1p10mean-Lucas-Kanto/src/app/inscription/inscription.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inscription',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrl: './inscription.component.css'
 })
 export class InscriptionComponent {
+  nom: string="";
+  email: string="";
+  phone: string="";
+  mdp: string="";
+  argent: number=0;
+  token: string = "";
 
+  constructor(private router: Router,private http: HttpClient)
+  {
+   
+  }
+
+  inscription() {
+    let bodyData = {
+      "nom" : this.nom,
+      "email" : this.email,
+      "phone" : this.phone,
+      "mdp" : this.mdp,
+      "argent": this.argent,
+      "token": this.token,
+
+    };
+    this.http.post("http://localhost:3000/client/create",bodyData,{responseType: 'text'}).subscribe((resultData: any)=>
+    {
+        console.log(resultData);
+        alert("Registered Successfully");
+
+        if (resultData.status) 
+        {
+      
+           this.router.navigateByUrl('/login');
+    
+
+        } 
+        else
+         {
+          console.log("Errror inscription");
+        }
+    });
+  }
 }
