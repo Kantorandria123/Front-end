14386d3cad01b234bb64144468890c658e381f65
LucasRazafinjatovo <96420763+LucasRazafinjatovo@users.noreply.github.com>
2024-02-05 11:18:40 +0300

    [Preference] liste employe et service

--- m1p10mean-Lucas-Kanto/src/app/preference/preference.component.ts
+++ m1p10mean-Lucas-Kanto/src/app/preference/preference.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-preference',
   templateUrl: './preference.component.html',
   styleUrl: './preference.component.css'
 })
-export class PreferenceComponent {
+export class PreferenceComponent implements OnInit{
+  services: any[] = [];
+  employes: any[] = [];
 
+
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.getListEmploye();
+    this.getListService();
+
+  }
+
+  getListService() {
+    const url = 'http://localhost:3000/service/lesservices';
+
+    this.http.get<any>(url).subscribe(
+      (response) => {
+        // La réponse contient la liste des services
+        if (response.status && response.services) {
+          this.services = response.services;
+          console.log('Liste des services :', this.services);
+        } else {
+          console.error('Réponse inattendue du serveur :', response);
+        }
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération de la liste des services :', error);
+      }
+    );
+  }
+  getListEmploye() {
+    const url = 'http://localhost:3000/employe/lesEmployes';
+
+    this.http.get<any>(url).subscribe(
+      (response) => {
+        if (response.status && response.employes) {
+          this.employes = response.employes;
+          console.log('Liste des services :', this.employes);
+        } else {
+          console.error('Réponse inattendue du serveur :', response);
+        }
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération de la liste des employes :', error);
+      }
+    );
+  }
 }
