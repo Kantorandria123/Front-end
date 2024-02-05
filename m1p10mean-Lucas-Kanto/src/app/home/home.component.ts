a39646821c1e3f6867e46f90b9a6d2429a33970e
LucasRazafinjatovo <96420763+LucasRazafinjatovo@users.noreply.github.com>
2024-02-05 10:11:54 +0300

    [Sercvices] liste service sur page accueil

--- m1p10mean-Lucas-Kanto/src/app/home/home.component.ts
+++ m1p10mean-Lucas-Kanto/src/app/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  services: any[] = [];
 
+  constructor(private http: HttpClient,private cookieService: CookieService) {}
+  ngOnInit() {
+    this.getListService();
+  }
+
+  getListService() {
+    const url = 'http://localhost:3000/service/lesservices';
+
+    this.http.get<any>(url).subscribe(
+      (response) => {
+        // La réponse contient la liste des services
+        if (response.status && response.services) {
+          this.services = response.services;
+          console.log('Liste des services :', this.services);
+        } else {
+          console.error('Réponse inattendue du serveur :', response);
+        }
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération de la liste des services :', error);
+      }
+    );
+  }
 }
