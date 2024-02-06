87042ed04e5bb92c1e48c8bf019a43e94449ce8e
LucasRazafinjatovo <96420763+LucasRazafinjatovo@users.noreply.github.com>
2024-02-06 21:31:27 +0300

    [Preference employe] insert

--- m1p10mean-Lucas-Kanto/src/app/preference/preference.component.ts
+++ m1p10mean-Lucas-Kanto/src/app/preference/preference.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-preference',
@@ -9,9 +10,11 @@ import { Component, OnInit } from '@angular/core';
 export class PreferenceComponent implements OnInit{
   services: any[] = [];
   employes: any[] = [];
-
-
-  constructor(private http: HttpClient) {}
+  employe_id:string="";
+  service_id:string="";
+  strongMessage:string="";
+  message:string="";
+  constructor(private http: HttpClient,private cookieService: CookieService) {}
   ngOnInit() {
     this.getListEmploye();
     this.getListService();
@@ -26,7 +29,6 @@ export class PreferenceComponent implements OnInit{
         // La réponse contient la liste des services
         if (response.status && response.services) {
           this.services = response.services;
-          console.log('Liste des services :', this.services);
         } else {
           console.error('Réponse inattendue du serveur :', response);
         }
@@ -43,7 +45,7 @@ export class PreferenceComponent implements OnInit{
       (response) => {
         if (response.status && response.employes) {
           this.employes = response.employes;
-          console.log('Liste des services :', this.employes);
+
         } else {
           console.error('Réponse inattendue du serveur :', response);
         }
@@ -53,4 +55,42 @@ export class PreferenceComponent implements OnInit{
       }
     );
   }
+  favoris(choix_id: any, type: any, nom: any) {
+    console.log("********mon favoris = " + choix_id);
+    const client_idCookie = this.cookieService.get('id');
+    if (client_idCookie) {
+      if (parseInt(type) === 1) {
+        console.log("==> service io");
+        this.service_id = choix_id;
+        let bodyData = {
+          "service_id": this.service_id,
+          "client_id": client_idCookie
+        };
+        console.log("bodyData emp : " + JSON.stringify(bodyData));
+        this.http.post("http://localhost:3000/preference/service/creer", bodyData).subscribe(
+          (resultData: any) => {
+            console.log("resulData.status = " + resultData.status);
+            if (resultData.status) {
+              this.strongMessage="C'est fait!";
+              this.message = "Nouveau favoris ajout(e)  : " + nom + ".";
+            }
+          },
+          (error: any) => {
+            console.error('Erreur lors de la requête HTTP :', error);
+            this.strongMessage="Attention! ";
+            this.message = "Cette service est déjà dans vos favoris";
+          }
+        );
+      }
+
+      if (parseInt(type) === 11) {
+        console.log("==> employe io");
+        // Mettez ici le code pour le type 11 si nécessaire
+      }
+
+    } else {
+      alert("Veuillez vous connecter");
+    }
+  }
+
 }
