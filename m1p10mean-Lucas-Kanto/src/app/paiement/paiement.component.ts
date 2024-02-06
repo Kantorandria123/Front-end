a6600e7387d8b75cdf546c443a28c71cf37afbfc
Kantorandria123 <kantomiharintsoarandria@gmail.com>
2024-02-06 22:15:21 +0300

    Rendez-vous[Paiement]

--- m1p10mean-Lucas-Kanto/src/app/paiement/paiement.component.ts
+++ m1p10mean-Lucas-Kanto/src/app/paiement/paiement.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { CookieService } from 'ngx-cookie-service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-paiement',
@@ -8,8 +10,14 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
 })
 export class PaiementComponent {
   title = 'angular-drag-drop-tutorial';
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  rendezvous: any[] = [];
+  payer: any[] = [];
+
+  constructor(private http: HttpClient,private cookieService: CookieService) {}
+
+  ngOnInit() {
+    this.getListPaiement();
+  }
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
@@ -23,6 +31,30 @@ export class PaiementComponent {
         event.previousIndex,
         event.currentIndex
       );
+    }
+  }
+
+  getListPaiement() {
+    const client_id = this.cookieService.get('id');
+    if (client_id) {
+      const url = `http://localhost:3000/rendezvous/lesrendezvous/${client_id}`;
+
+      this.http.get<any>(url).subscribe(
+        (response) => {
+          // La réponse contient la liste des rendezvous
+          if (response.status && response.rendezvousList) {
+            this.rendezvous = response.rendezvousList;
+            console.log('Liste des rendezvous :', this.rendezvous);
+          } else {
+            console.error('Réponse inattendue du serveur :', response);
+          }
+        },
+        (error) => {
+          console.error('Erreur lors de la récupération de la liste des rendezvous :', error);
+        }
+      );
+    } else {
+      console.error('client_id non trouvé dans le cookie.');
     }
   }
 
