1502e06b293179ebc192f625699f5bc755234d86
LucasRazafinjatovo <96420763+LucasRazafinjatovo@users.noreply.github.com>
2024-02-12 20:35:42 +0300

    [ENV]

--- m1p10mean-Lucas-Kanto/src/app/environments/environment.ts
+++ m1p10mean-Lucas-Kanto/src/app/environments/environment.ts
@@ -2,5 +2,5 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://m1p10mean-lucas-kanto.onrender.com' // Remplacez "http://example.com/api" par votre URL de base
+  baseUrl: 'http://localhost:3000' // Remplacez "http://example.com/api" par votre URL de base
 };
