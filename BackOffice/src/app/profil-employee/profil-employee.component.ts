97d38710c95c28d2ab5b48110bc64f309fdce79f
Kantorandria123 <kantomiharintsoarandria@gmail.com>
2024-02-13 22:54:58 +0300

    details-Employee

--- BackOffice/src/app/profil-employee/profil-employee.component.ts
+++ BackOffice/src/app/profil-employee/profil-employee.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component,OnInit  } from '@angular/core';
+import { error } from 'console';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-profil-employee',
   templateUrl: './profil-employee.component.html',
   styleUrl: './profil-employee.component.css'
 })
-export class ProfilEmployeeComponent {
+export class ProfilEmployeeComponent implements OnInit{
+  employees: any[] = [];
+
+  constructor(private http: HttpClient,private cookieService: CookieService) {}
+  ngOnInit(): void {
+    this.listeEmployeeById();
+  }
+
+  listeEmployeeById() {
+    const employeeId = this.cookieService.get('id');
+     if(employeeId) {
+      const url = `http://localhost:3000/employe/employebyId/${employeeId}`;
+
+      this.http.get<any>(url).subscribe(
+        (response) => {
+          if (response.status && response.employes) {
+            // Check if employes is an array, if not, convert it to an array
+            if (Array.isArray(response.employes)) {
+              this.employees = response.employes;
+            } else {
+              // If employes is an object, convert it to an array
+              this.employees = [response.employes];
+            }
+            console.log("Liste employées :", this.employees);
+          } else {
+            console.error('Réponse inattendue du serveur :', response);
+          }
+        },
+        (error) => {
+          console.error('Erreur lors de la récupération de la liste des employées :', error);
+        }
+      );
+     } else {
+      console.error('employeeId non trouvé dans le cookie.');
+     }
+  }
 
 }
