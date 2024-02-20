e1da8d5fa133d6e7cf51e0ce3aa44c4a23f1e95a
Kantorandria123 <kantomiharintsoarandria@gmail.com>
2024-02-20 12:38:23 +0300

    Service-ajout

--- BackOffice/src/app/service/service.component.ts
+++ BackOffice/src/app/service/service.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-service',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrl: './service.component.css'
 })
 export class ServiceComponent {
+  nom: string="";
+  description: string="";
+  image: string="";
+  prix: string="";
+  duree: string="";
+  commission: string="";
+  strongMessage: string="";
+
+  constructor(private router: Router,private http: HttpClient){}
+
+  serviceCreate() {
+    console.log('service');
+    let bodyData = {
+      "nom": this.nom,
+      "description": this.description,
+      "image": this.image,
+      "prix": this.prix,
+      "duree": this.duree,
+      "commission": this.commission,
+    };
+    console.log("bodyData : "+bodyData);
+    this.http.post(environment.baseUrl+"/service/creer",bodyData).subscribe((resultData: any)=> {
+      if (resultData.status){
+        this.strongMessage="Service créer"
+      } else {
+        this.strongMessage="Service non créer"
+      }
+    });
+  }
 
 }
