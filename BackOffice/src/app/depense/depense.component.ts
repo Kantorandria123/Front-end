3fb427ffca65d3ae5d7a179a0fe1b550c2115c8e
Kantorandria123 <kantomiharintsoarandria@gmail.com>
2024-02-20 23:47:16 +0300

    [Depense]-create

--- BackOffice/src/app/depense/depense.component.ts
+++ BackOffice/src/app/depense/depense.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-depense',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrl: './depense.component.css'
 })
 export class DepenseComponent {
+  description: string="";
+  montant: string="";
+  date: string="";
+  type: string="";
+
+  constructor(private router: Router,private http: HttpClient) {}
+
+  depenseCreate() {
+    const bodyData = {
+      description: this.description,
+      montant: this.montant,
+      date: this.date,
+      type: this.type,
+    };
+
+    this.http.post(environment.baseUrl + '/depense/creer',bodyData).subscribe(
+      (resultData: any) => {
+      if(resultData.status) {
+        window.location.reload();
+      } else {
+        console.error('Réponse inattendue du serveur');
+      }
+    },
+    (error) => {
+      console.error('Error creating dépense:', error);
+    }
+    
+    );
+  }
 
 }
